61cabeaf738aa5ce6ace0d2123aa6564ebbdfbe5
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-23 00:08:40 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand) == false BRANDS << brand
+    if BRANDS.include?(brand) == false 
+      BRANDS << brand
     end
     
   end
